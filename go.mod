0f9b6068b00f66ac15991985a3bc18cb780e07c9
hihimamuLab <hihimamu14@outlook.jp>
2023-01-29 23:09:39 +0900

    Co-authored-by: ikafly144 <support@sabafly.net>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hihimamuLab/go-projects
 
 go 1.19
+
+require golang.org/x/tour v0.1.0
--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func Abs(v *Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func Scale(v Vertex, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
 
 func main() {
-	fmt.Println("HelloWorld!")
+	v := Vertex{3, 4}
+	Scale(v, 10)
+	fmt.Println(Abs(&v))
 }
