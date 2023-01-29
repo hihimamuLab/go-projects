83e7fd2a4da1e553dd8bf4a47f867ad92a38f35c
hihimamuLab <hihimamu14@outlook.jp>
2023-01-30 00:32:54 +0900

    適当に

--- main.go
+++ main.go
@@ -1,40 +1,23 @@
 package main
 
-import "fmt"
-
-type StringSlice []string
-
-func (s StringSlice) Len() int {
-	return len(s)
-}
-
-type IntSlice []int
-
-func (i IntSlice) Len() int {
-	return len(i)
-}
-
-type Slice interface {
-	Len() int
-}
-
-type FloatSlice []float64
-
-func (f FloatSlice) Len() int {
-	return len(f)
-}
-
-func print(s Slice) {
-	for i := 0; i < s.Len(); i++ {
-		fmt.Println(s)
-	}
-}
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	s := StringSlice{"foo", "bar", "fuga", "hoge", "piyo"}
-	print(s)
-	i := IntSlice{1, 2, 3, 4, 5}
-	print(i)
-	f := FloatSlice{1, 2, 3, 4, 5}
-	print(f)
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("BOOM!")
+			return
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
 }
